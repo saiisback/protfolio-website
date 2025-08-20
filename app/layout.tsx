9a85179f56be89d1ac139691dd75e230760b597f
sai karthik <saikarthik@Mac.lan>
2025-08-21 05:04:05 +0530

    fixed the improvise

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Playfair_Display, Merriweather } from "next/font/google"
 import "./globals.css"
+import { Analytics } from '@vercel/analytics/next';
 
 const playfair = Playfair_Display({
   subsets: ["latin"],
@@ -30,7 +31,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${playfair.variable} ${merriweather.variable}`}>
-      <body>{children}</body>
+      <body>{children}
+        <Analytics />
+      </body>
     </html>
   )
 }
